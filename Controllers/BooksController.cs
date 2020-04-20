44101d3a0f3e7969379323c4d42d3f9a876a2a37
Alison Tomsho <alison.tomsho@gmail.com>
2020-04-20 09:23:57 -0400

    commit

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,8 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Fisher.Bookstore.Services;
+
 namespace Fisher.Bookstore.Controllers
 {
-
-    public class BooksController
+    [ApiController]
+    [Route("api/[controller]")]
+    
+    public class BooksController : ControllerBase
     {
-
+    private IBooksRepository booksRepository;
+    public BooksController(IBooksRepository repository){
+        booksRepository = repository;
+    }
+    [HttpGet]
+    public IActionResult GetAll(){
+        return Ok(booksRepository.GetBooks());
+    }
     }
 }
